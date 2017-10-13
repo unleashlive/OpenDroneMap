0ecd66c77b6771b1d12b668f63dea3b58e54459c
mordka <pawel.smile@gmail.com>
2017-10-13 21:23:02 +0100

    zip results files

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from opendm import log
 from opendm import config
 from opendm import system
 from opendm import io
+import zip
 
 import ecto
 import os
@@ -45,5 +46,9 @@ if __name__ == '__main__':
 
     # execute the plasm
     plasm.execute(niter=1)
+    zip.zip_dirs([args.project_path + "/odm_georeferencing",
+                    args.project_path + "/odm_meshing",
+                    args.project_path + "/odm_orthophoto",
+                    args.project_path + "/odm_texturing"])
 
     log.ODM_INFO('OpenDroneMap app finished - %s' % system.now())
---
+++ zip.py
@@ -0,0 +1,16 @@
+import os
+import zipfile
+
+def zipdir(foldername, ziph):
+    for root, dirs, files in os.walk(foldername):
+        for file in files:
+            # add files to the archive directly
+            ziph.write(os.path.abspath(os.path.join(root, file)), arcname=file)
+
+# expect files to be an array of direct result folder paths
+def zip_dirs(dirs):
+    print(dirs)
+    for dir in dirs:
+        zipf = zipfile.ZipFile(dir+'.zip', 'w', zipfile.ZIP_DEFLATED)
+        zipdir(dir+'/', zipf)
+        zipf.close()
