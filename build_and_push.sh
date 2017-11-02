728baf0e02572a4ba5b74e818e12c89a3c332328
mordka <pawel.smile@gmail.com>
2017-11-02 17:51:52 +0000

    add build script

---
+++ build_and_push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+eval $(aws ecr get-login --no-include-email --region ap-southeast-2 | sed 's|https://||')
+docker build --no-cache -t odm .
+docker tag odm:latest 712356514005.dkr.ecr.ap-southeast-2.amazonaws.com/imhotep:odm
+docker push 712356514005.dkr.ecr.ap-southeast-2.amazonaws.com/imhotep:odm
