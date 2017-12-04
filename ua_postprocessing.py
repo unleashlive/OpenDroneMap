81a6e9cc2803c9813b17ca82b602275d569c44e2
mordka <pawel.smile@gmail.com>
2017-12-04 20:52:22 +0100

    create 2x smaller textures instead of 8x smaller

--- ua_postprocessing.py
+++ ua_postprocessing.py
@@ -4,17 +4,17 @@ def resize_textures(project_path):
     outputGLTFFile = project_path + "/odm_texturing/odm_textured_model.gltf"
     outputBINFile = project_path + "/odm_texturing/odm_textured_model.bin"
     odm_texturing_folder = project_path + "/odm_texturing"
-    resized_8x_folder = "resized_8x"
+    resized_2x_folder = "resized_2x"
     resized_4x_folder = "resized_4x"
     # due to imagemagick bug, its impossible to have colon : in the -path param content, so use 'cd' to get relative path
-    resize_8x_command = 'cd ' + odm_texturing_folder + ' && mkdir ' + resized_8x_folder + ' && mogrify -adaptive-resize 12.5% -path ' + resized_8x_folder + ' -format png ' + './*.png'
+    resize_2x_command = 'cd ' + odm_texturing_folder + ' && mkdir ' + resized_2x_folder + ' && mogrify -adaptive-resize 50% -path ' + resized_2x_folder + ' -format png ' + './*.png'
     resize_4x_command = 'cd ' + odm_texturing_folder + ' && mkdir ' + resized_4x_folder + ' && mogrify -adaptive-resize 25% -path ' + resized_4x_folder + ' -format png ' + './*.png'
-    os.system(resize_8x_command)
+    os.system(resize_2x_command)
     os.system(resize_4x_command)
     #copy gltf and bin files, because they use relative paths to textures
-    os.system('cp ' + outputGLTFFile + ' ' + odm_texturing_folder + '/' + resized_8x_folder)
+    os.system('cp ' + outputGLTFFile + ' ' + odm_texturing_folder + '/' + resized_2x_folder)
     os.system('cp ' + outputGLTFFile + ' ' + odm_texturing_folder + '/' + resized_4x_folder)
-    os.system('cp ' + outputBINFile + ' ' + odm_texturing_folder + '/' + resized_8x_folder)
+    os.system('cp ' + outputBINFile + ' ' + odm_texturing_folder + '/' + resized_2x_folder)
     os.system('cp ' + outputBINFile + ' ' + odm_texturing_folder + '/' + resized_4x_folder)
 
 def obj2gltf(project_path):
