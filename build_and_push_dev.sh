59c522a74b93ae05419767aee162b7e5a2048979
mordka <pawel@unleashlive.com>
2020-01-10 15:14:28 +0100

    build: add git pull

--- build_and_push_dev.sh
+++ build_and_push_dev.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+git pull
 docker build -t odm:latest .
 eval $(aws ecr get-login --no-include-email --region ap-southeast-2 | sed 's|https://||')
 docker tag odm:latest 712356514005.dkr.ecr.ap-southeast-2.amazonaws.com/odm:latest
--- build_and_push_prod.sh
+++ build_and_push_prod.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+git pull
 docker build -t odm:stable .
 eval $(aws ecr get-login --no-include-email --region ap-southeast-2 | sed 's|https://||')
 docker tag odm:stable 712356514005.dkr.ecr.ap-southeast-2.amazonaws.com/odm:stable
